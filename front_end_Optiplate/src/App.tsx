9dff6afed1be25d6ea6c82eab1c20de8137d8228
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-17 16:13:13 -0500

    cambviar entre modo oscuro y claro

--- front_end_Optiplate/src/App.tsx
+++ front_end_Optiplate/src/App.tsx
@@ -1,21 +1,22 @@
-// import { useEffect } from 'react'
-
+import { useState } from 'react'
 import './App.css'
-
 import VerticalSkip from './VerticalSkip'
 import TitleAndBrand from './TitleAndBrand';
 import LoginButton from './LoginButton'
 import Fondo from './Fondo';
 import FondoDia from './FondoDia';
 
-
 function App() {
-  
-  
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleMode = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
   return (
     <>
       <div className="relative h-screen w-screen">
-        <FondoDia/>
+        {isDarkMode ? <Fondo /> : <FondoDia />}
         <div className="relative flex flex-col items-center h-full w-full">
           <VerticalSkip skip={30} logo={true}/>  
           <TitleAndBrand />
@@ -27,10 +28,15 @@ function App() {
         <a className='fixed top-2/3 left-8 lg:left-24'>
           <img className='lg:scale-150' src="/keyboardApp.png" />
         </a>
+        <button 
+          className="fixed bottom-4 right-4 bg-gray-800 text-white px-4 py-2 rounded-lg"
+          onClick={toggleMode}
+        >
+          {isDarkMode ? 'Modo Claro' : 'Modo Oscuro'}
+        </button>
       </div>
     </>
-  )
-
+  );
 }
 
 export default App
