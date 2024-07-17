572d61daa3ba15be58ca9d11502237b94349f41f
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-07-17 15:50:37 -0500

    Correccion de FondoDia.tsx

--- front_end_Optiplate/src/FondoDia.tsx
+++ front_end_Optiplate/src/FondoDia.tsx
@@ -1,109 +1,84 @@
-function FondoDia(){
+function FondoDia() {
   return (
     <div className="absolute inset-0">
-<div className="frame">
-  <link rel="stylesheet" href="./fondo-dia.css"/>
-    <div className="pyramids">
-      <div className="pyramid pyramid-giza"></div>
-      <div className="pyramid pyramid-Khafre"></div>
-      <div className="pyramid pyramid-menkaure"></div>
-    </div>
-    <div className="sky">
-      <div id="background-wrap">
-        <div className="x1">
-          <div className="cloud"></div>
+      <div className="frame">
+        <link rel="stylesheet" href="./fondo-dia.css" />
+        <div className="pyramids">
+          <div className="pyramid pyramid-giza"></div>
+          <div className="pyramid pyramid-Khafre"></div>
+          <div className="pyramid pyramid-menkaure"></div>
         </div>
-        <div className="x2">
-          <div className="cloud"></div>
-        </div>
-        <div className="x3">
-          <div className="cloud"></div>
-        </div>
-        <div className="x4">
-          <div className="cloud"></div>
-        </div>
-        <div className="x5">
-          <div className="cloud"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className="wind">
-    <div className="wind-instance" style="top: 10%; left: 20%;">
-      <div className="circle"></div>
-      <div className="center">
-        <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
-          <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
-            c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
-            c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
-            c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
-            c17.6-17.9,40-24.6,56.4-27.5"/>
-        </svg>
-      </div>
-    </div>
-    <div className="wind-instance" style="top: 60%; left: 30%;">
-      <div className="circle"></div>
-      <div className="center">
-        <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
-          <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
-            c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
-            c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
-            c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
-            c17.6-17.9,40-24.6,56.4-27.5"/>
-        </svg>
-      </div>
-    </div>
-    <div className="wind-instance" style="top: 20%; left: 70%;">
-      <div className="circle"></div>
-      <div className="center">
-        <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
-          <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
-            c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
-            c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
-            c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
-            c17.6-17.9,40-24.6,56.4-27.5"/>
-        </svg>
-      </div>
-    </div>
-    <div className="wind-instance" style="top: 50%; left: 80%;">
-      <div className="circle"></div>
-      <div className="center">
-        <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
-          <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
-            c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
-            c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
-            c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
-            c17.6-17.9,40-24.6,56.4-27.5"/>
-        </svg>
-      </div>
-    </div>
-    <div className="wind-instance" style="top: 40%; left: 50%;">
-      <div className="circle"></div>
-      <div className="center">
-        <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
-          <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
-            c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
-            c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
-            c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
-            c17.6-17.9,40-24.6,56.4-27.5"/>
-        </svg>
-      </div>
-    </div>
-    <div className="wind-instance" style="top: 70%; left: 10%;">
-      <div className="circle"></div>
-      <div className="center">
-        <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
-          <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
-            c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
-            c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
-            c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
-            c17.6-17.9,40-24.6,56.4-27.5"/>
-        </svg>
-      </div>
-    </div>
-  </div>
+        <div className="sky">
+          <div id="background-wrap">
+            <div className="x1">
+              <div className="cloud"></div>
             </div>
-  )
+            <div className="x2">
+              <div className="cloud"></div>
+            </div>
+            <div className="x3">
+              <div className="cloud"></div>
+            </div>
+            <div className="x4">
+              <div className="cloud"></div>
+            </div>
+            <div className="x5">
+              <div className="cloud"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wind">
+        <div className="wind-instance" style={{ top: '10%', left: '20%' }}>
+          <div className="circle"></div>
+          <div className="center">
+            <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
+              <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
+                c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
+                c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
+                c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
+                c17.6-17.9,40-24.6,56.4-27.5"/>
+            </svg>
+          </div>
+        </div>
+        <div className="wind-instance" style={{ top: '60%', left: '30%' }}>
+          <div className="circle"></div>
+          <div className="center">
+            <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
+              <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
+                c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
+                c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
+                c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
+                c17.6-17.9,40-24.6,56.4-27.5"/>
+            </svg>
+          </div>
+        </div>
+        <div className="wind-instance" style={{ top: '20%', left: '70%' }}>
+          <div className="circle"></div>
+          <div className="center">
+            <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
+              <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
+                c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
+                c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5
+                c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7
+                c17.6-17.9,40-24.6,56.4-27.5"/>
+            </svg>
+          </div>
+        </div>
+        <div className="wind-instance" style={{ top: '50%', left: '80%' }}>
+          <div className="circle"></div>
+          <div className="center">
+            <svg stroke="#fff" width="400px" height="400px" viewBox="0 0 400 400" version="1.1" className="line">
+              <path className="st0" d="M-34.8,166.8c51.6,1.9,70.9,16.4,78.4,30.3c10.9,20.1-3.6,37.5,6.9,75c3.9,14,11.8,42.2,33.7,50.9
+                c25.7,10.2,65.3-8.7,76.4-36.5c13.8-34.4-26.4-57.5-19.3-110.1c3.3-24.1,17.1-58.7,44.7-67.4c34.9-10.9,84.3,21.7,90.8,65.4
+                c5.3,36-20.6,66.1-42.6,79.8c-39.5,24.5-95.7,14.9-108-7.6c-0.5-0.9-7.9-14.7-2.1-22.7c3.7-5,10.9-5.4,13.1-5.5c15.4-0.8,28.1,12.1,33,17.2c46.5,48.5,53.9,63.9,72.2,75.7
+                c24.1,15.6,66.1,24.2,90.8,6.9c36.4-25.5,7.6-88,49.5-130.7c17.6-17.9,40-24.6,56.4-27.5"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default FondoDia
+export default FondoDia;
