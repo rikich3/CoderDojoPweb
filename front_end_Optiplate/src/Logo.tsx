af121b20f58003dcec0da6431d543275fe9409a3
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-17 18:15:11 -0500

    Subiendo cambios de lab

--- front_end_Optiplate/src/Logo.tsx
+++ front_end_Optiplate/src/Logo.tsx
@@ -1,9 +1,12 @@
 const Logo = (props: { logo: any }) => {
   if(props.logo){
     return (
+      <div >
       <a href="https://coderdojo.com/" target="_blank">
       <img src="/coderdojo-logo.png" className="h-20"/>
+      <img src="/unsa-logo.png" className="h-20"/>
       </a>
+      </div>
     )
   } else {
     return (<></>)
